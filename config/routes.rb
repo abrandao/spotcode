8153b71af471abea01c4b61458e8cc93f311648b
abrandao <andersonbrandaolustosa@gmail.com>
2019-02-11 18:20:22 -0200

    Favorite songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  
   authenticated :user do
-    root to: 'dashboard#index', as: :authenticated_root
+    resources :favorites, only: :index
+    root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
     resources :categories, only: :show
     resources :artists, only: :show
     resources :albums, only: :show
+    resources :songs, only: [] do
+      post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+      delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+    end
   end
 
   unauthenticated :user do
-    root to: 'home#index'
-  end 
+    root to: "home#index"
+  end
 end
